046431aba6378dc664c72b83f18758e3e835a8f8
Fredrik Bjureström <Fredrik.Bjurestrom@storm.io>
2018-04-10 12:13:29 +0200

    2.2.20-beta2: Added CompetitorSkuHeader and fields

---
+++ Enferno.Services.StormConnect.Contracts/Product/Fields/CompetitorSkuField.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace Enferno.Services.StormConnect.Contracts.Product.Fields
+{
+    [DataContract(Name = "CompetitorSkuField", Namespace = "Enferno.Services.StormConnect.Contracts.Product.Fields")]
+    public enum CompetitorSkuField
+    {
+        [EnumMember]
+        Source = 0,
+        [EnumMember]
+        CompetitorCode = 1,
+        [EnumMember]
+        ProductName = 2,
+        [EnumMember]
+        VatRate = 3,
+        [EnumMember]
+        ExternalPopularities = 4
+    }
+}

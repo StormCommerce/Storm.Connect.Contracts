074f910efc75d778a78fcaab31c41fa24a33f207
Peter Haraldsson <peter.haraldsson@storm.io>
2019-04-12 13:25:04 +0200

    #5042 Development of ExternalOrder and Invoice

--- Enferno.Services.StormConnect.Contracts/Order/ExternalOrder.cs
+++ Enferno.Services.StormConnect.Contracts/Order/ExternalOrder.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace Enferno.Services.StormConnect.Contracts.Order
 {
@@ -8,34 +9,32 @@ namespace Enferno.Services.StormConnect.Contracts.Order
         [DataMember(Order = 1)]
         public Header Header { get; set; }
         [DataMember(Order = 2)]
-        public ClientCustomer SellTo { get; set; }
+        public DateTime OrderDate { get; set; }
         [DataMember(Order = 3)]
-        public ClientCustomer BillTo { get; set; }
+        public ClientCustomer SellTo { get; set; }
         [DataMember(Order = 4)]
-        public ClientCustomer ShipTo { get; set; }
+        public ClientCustomer BillTo { get; set; }
         [DataMember(Order = 5)]
-        public string SalesDivisionCode { get; set; }
+        public ClientCustomer ShipTo { get; set; }
         [DataMember(Order = 6)]
-        public string DeliveryDivisionCode { get; set; }
+        public string SalesDivisionCode { get; set; }
         [DataMember(Order = 7)]
-        public string ErpOrderNo { get; set; }
+        public string DeliveryDivisionCode { get; set; }
         [DataMember(Order = 8)]
-        public string CustomerOrderRef { get; set; }
+        public string ErpOrderNo { get; set; }
         [DataMember(Order = 9)]
-        public string CustomerOrderComment { get; set; }
+        public string CustomerOrderRef { get; set; }
         [DataMember(Order = 10)]
-        public Payments Payments { get; set; }
+        public string CustomerOrderComment { get; set; }
         [DataMember(Order = 11)]
-        public Discounts Discounts { get; set; }
+        public Invoices Invoices { get; set; }
         [DataMember(Order = 12)]
         public ShippingAdvice ShippingAdvice { get; set; }
         [DataMember(Order = 13)]
         public OrderItems Items { get; set; }
         [DataMember(Order = 14)]
-        public Fees Fees { get; set; }
-        [DataMember(Order = 15)]
         public CodeValues AdditionalInfo { get; set; }
-        [DataMember(Order = 16)]
+        [DataMember(Order = 15)]
         public string Source { get; set; }
         public virtual ExtensionDataObject ExtensionData { get; set; }
     }
---
+++ Enferno.Services.StormConnect.Contracts/Order/Invoices.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+namespace Enferno.Services.StormConnect.Contracts.Order
+{
+    public class Invoices : List<Invoice> { }
+}

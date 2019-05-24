2fe23dce8babbcaaa46ca5f86ac3dc2f3527639a
Joar Selving <joar.selving@storm.io>
2019-05-24 11:28:02 +0200

    Added OrderPaymentRequest

---
+++ Enferno.Services.StormConnect.Contracts/Order/OrderPaymentRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Enferno.Services.StormConnect.Contracts.Common;
+
+namespace Enferno.Services.StormConnect.Contracts.Order
+{
+    [DataContract(Name = "OrderPaymentRequest", Namespace = "Enferno.Services.StormConnect.Contracts.Order")]
+    public class OrderPaymentRequest : IExtensibleDataObject
+    {
+        [DataMember(Order = 1)]
+        public Header Header { get; set; }
+        [DataMember(Order = 2)]
+        public string ClientOrderRef { get; set; }
+        [DataMember(Order = 3)]
+        public string ErpOrderNo { get; set; }
+        [DataMember(Order = 4)]
+        public Payment Payment { get; set; }
+        public virtual ExtensionDataObject ExtensionData { get; set; }
+    }
+}

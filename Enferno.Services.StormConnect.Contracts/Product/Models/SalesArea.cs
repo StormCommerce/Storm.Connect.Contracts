79e2d9c4681b5e93ea9ec19fa2b72dce9859164a
Peter Haraldsson <peter.haraldsson@storm.io>
2021-07-23 08:20:23 +0200

    DEV-1011: Updated sales areas

--- Enferno.Services.StormConnect.Contracts/Product/Models/SalesArea.cs
+++ Enferno.Services.StormConnect.Contracts/Product/Models/SalesArea.cs
@@ -42,5 +42,47 @@ namespace Enferno.Services.StormConnect.Contracts.Product.Models
         Ireland = 18,
         [EnumMember]
         Canada = 19,
+        [EnumMember]
+        Lithuania = 20,
+        [EnumMember]
+        Latvia = 21,
+        [EnumMember]
+        Japan = 22,
+        [EnumMember]
+        SouthKorea = 23,
+        [EnumMember]
+        Australia = 24,
+        [EnumMember]
+        Europe = 25,
+        [EnumMember]
+        Poland = 26,
+        [EnumMember]
+        CzechRepublic = 27,
+        [EnumMember]
+        Russia = 28,
+        [EnumMember]
+        NoVAT = 29,
+        [EnumMember]
+        Bulgaria = 36,
+        [EnumMember]
+        Croatia = 37,
+        [EnumMember]
+        Cyprus = 38,
+        [EnumMember]
+        Greece = 39,
+        [EnumMember]
+        Hungary = 40,
+        [EnumMember]
+        Luxembourg = 41,
+        [EnumMember]
+        Malta = 42,
+        [EnumMember]
+        Portugal = 43,
+        [EnumMember]
+        Romania = 44,
+        [EnumMember]
+        Slovakia = 45,
+        [EnumMember]
+        Slovenia = 46
     }
 }

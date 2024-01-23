80ddc6e0c4ffeccaf23f6f36d848a56a2948716e
Patrik Nilsson <patrik.nilsson@norce.io>
2024-01-23 11:45:54 +0100

    added localized name and description to image

--- Enferno.Services.StormConnect.Contracts/Common/Image.cs
+++ Enferno.Services.StormConnect.Contracts/Common/Image.cs
@@ -8,13 +8,23 @@ namespace Enferno.Services.StormConnect.Contracts.Common
     {
         [DataMember(Order = 1)]
         public string FileTypeName { get; set; }
+
         [DataMember(Order = 2)]
         public string ExternalCode { get; set; }
+
         [DataMember(Order = 3)]
         public string Filename { get; set; }
+
         [DataMember(Order = 4)]
         public string Url { get; set; }
+
         [DataMember(Order = 5)]
         public DateTime? LastModified { get; set; }
+
+        [DataMember(Order = 6)]
+        public LocalizedString Name { get; set; }
+
+        [DataMember(Order = 7)]
+        public LocalizedString Description { get; set; }
     }
 }

43c0c781e27a4b19a9c1154c18121ddcdaf45a20
Joel Nygren <joel.nygren@norce.io>
2024-02-07 14:00:37 +0100

    added name and description to ImageCultureField

---
+++ Enferno.Services.StormConnect.Contracts/Product/Fields/ImageCultureField.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Enferno.Services.StormConnect.Contracts.Product.Fields
+{
+    [DataContract(Name = "ImageCultureField", Namespace = "Enferno.Services.StormConnect.Contracts.Product.Fields")]
+    public enum ImageCultureField
+    {
+        [EnumMember] Name = 0,
+        [EnumMember] Description = 1,
+      
+    }
+}
